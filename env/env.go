c53abb41fe6c2a17d5eb7e33c4f6f61bac5d4936
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2018-07-18 17:52:16 +0100

    Fix issue with pointer parsing

--- env/env.go
+++ env/env.go
@@ -1,25 +1,40 @@
 package env
 
 import (
-	"github.com/EVODelavega/goq/aws"
+	"os"
+
+	goqaws "github.com/EVODelavega/goq/aws"
 
 	"github.com/caarlos0/env"
 )
 
 // Get parses the variable environment and creates the config object
-func Get() (*aws.Config, error) {
+func Get() (*goqaws.Config, error) {
 
-	credentials := &aws.Credentials{}
+	credentials := &goqaws.Credentials{}
 	if err := env.Parse(credentials); err != nil {
 		return nil, err
 	}
 
-	config := &aws.Config{}
+	config := &goqaws.Config{}
 	if err := env.Parse(config); err != nil {
 		return nil, err
 	}
 
+	// Parse manually properties, that are presented as pointers
+	config.QueueUrl = getStringPtr(os.Getenv("AWS_QUEUE_URL"))
+	config.TopicArn = getStringPtr(os.Getenv("AWS_TOPIC_ARN"))
+
 	config.Credentials = credentials
 
 	return config, nil
+}
+
+// Optional properties are presented as poitners allowing to have nil's when value were not provided.
+// This simple function returns pointer only in case when string value is not empty
+func getStringPtr(v string) *string {
+	if len(v) > 0 {
+		return &v
+	}
+	return nil
 }
